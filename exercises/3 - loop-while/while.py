f17a668a72ada9f4d062dbd9987b39543497bdd9
thiagooshiro <oshirothiago@gmail.com>
2024-05-13 15:29:00 -0300

    exercises while loop - LP

---
+++ exercises/3 - loop-while/while.py
@@ -0,0 +1,30 @@
+# Exercícios Laços de Repetição - while.
+
+# 1 - Escreva um programa que recebe um número inteiro do usuário e então conte de volta desse número até 1, imprimindo cada um dos números entre o valor recebido e 1.
+
+# 2- Crie um algoritmo que recebe um número, faz o somatório de todos os valores até este número e exibe o somatório final no terminal.  Ex: n=3  o resutlado deverá ser 6 (1 + 2 + 3)
+
+# 3 - Escreva um programa que solicite repetidamente ao usuário para digitar uma senha até que eles insiram a senha correta, escolhida por você.
+
+
+# 4 - Escreva um programa que verifica se um número fornecido pelo usuário é primo ou não.
+
+# 5 - Escreva um programa que conte o número de vogais em uma palavra fornecida pelo usuário.
+
+# 6 - Considere a seguinte lista:
+livros = [
+    "O Pequeno Príncipe",
+    "Dom Quixote",
+    "Cem Anos de Solidão",
+    "1984",
+    "Orgulho e Preconceito",
+    "O Senhor dos Anéis",
+    "Harry Potter e a Pedra Filosofal",
+    "A Menina que Roubava Livros",
+    "Crime e Castigo",
+    "As Crônicas de Nárnia"
+]
+
+# Escreva um programa que filtra os livros a partir de uma letra fornecida como entrada pelo usuário. Por exemplo, caso o usuário digite a letra "a" todos os livros que contenham a letra "a" devem aparecer no resultado final.
+
+# 7 - Escreva um algoritmo que recebe como entrada um produto, armezena essa informação numa lista e pergunta ao usuário se ele deseja adicionar novos itens à esa lista, quando o usuário não desejar mais adicionar itens, os nomes itens adicionados devem ser impressos no terminal em forma de lista.
