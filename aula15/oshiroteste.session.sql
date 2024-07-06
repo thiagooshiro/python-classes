2bc28a62b8e46606682888a6eb2f9db1dc3456f7
thiagooshiro <oshirothiago@gmail.com>
2024-07-06 13:08:49 -0300

    add new stuff

--- aula15/oshiroteste.session.sql
+++ aula15/oshiroteste.session.sql
@@ -7,3 +7,13 @@ CREATE TABLE STUDENTS(
 );
 
 -- @block
+INSERT INTO Students.students(username, email, class_i)
+VALUES (
+    'oshiroteste',
+    'teste@email.com',
+    32
+)
+
+-- @block
+
+SELECT * FROM students
